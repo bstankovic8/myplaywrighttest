9448133b251801c21cc3cd603318037ca3a04491
Bora <bstankovic@knowit.rs>
2025-03-01 08:23:38 +0100

    All over again in TS, new tests

---
+++ tests/workshop_6/workshop_6.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from "@playwright/test";
+
+test.describe('User registration tests', () => {
+
+    test('Register with valid data', async ({page}) => {
+        
+    })
+})
