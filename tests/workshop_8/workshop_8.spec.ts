ac95a7941e4e39fca8fa819feb1a71f8cc80dfa0
Bora <bstankovic@knowit.rs>
2025-03-05 11:00:00 +0100

    All over again in TS, new tests

--- tests/workshop_8/workshop_8.spec.ts
+++ tests/workshop_8/workshop_8.spec.ts
@@ -10,7 +10,7 @@ test.describe('Sample Test', ()=>{
         await pageObject.open('http://127.0.0.1:5500/tests/workshop_8/index.html');
     })
 
-    test('Test 1: Fill all inputs',async ()=>{
+    test.skip('Test 1: Fill all inputs',async ()=>{
         await pageObject.fillFirstName('John');
         await pageObject.fillAge('30');
         await pageObject.checkIsStudent();
